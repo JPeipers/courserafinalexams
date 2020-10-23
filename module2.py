732ef158177e67032cc86ea2f0b81da0ea64fd3b
JP <janis.peipers@gmail.com>
2020-10-23 12:11:12 +0300

    Added module2 exam code. fixed #! line in module1.py by adding whitespace

---
+++ module2.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+import json
+import os
+import requests
+import sys
+
+txt_directory = 'module2_review/'
+destination_directory = 'module2_json/'
+revlist = []
+for file in os.listdir(txt_directory):
+    try:
+        with open(txt_directory + file, 'r') as review:
+            content = []
+            for line in review.readlines():
+                content.append(line.strip())
+            print(content)
+            revlist.append({'title': content[0], 'name': content[1], 'date': content[2], 'feedback': ''.join(content[3:len(content)])})
+    except:
+        print("Problems processing file: " + file + " Error type: ", sys.exc_info()[0])
+print(revlist)
+with open(destination_directory + 'reviews.json', 'w') as reviews_json:
+    json.dump(revlist, reviews_json, indent=2)
+# Lines for exam:
+# response = requests.post("[URL]]/feedback", json=revlist)
+# print(response.status_code)
